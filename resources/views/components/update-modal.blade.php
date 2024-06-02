760eb3812906e275347034b3b1af3c4379563c3a
Patrickstvn <146690535+JesslynLevana@users.noreply.github.com>
2024-06-03 00:53:42 +0700

    components

---
+++ resources/views/components/update-modal.blade.php
@@ -0,0 +1,51 @@
+@props(['id'])
+
+<div id="update-modal{{ $id }}" aria-hidden="true"
+    class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+    <div class="relative p-4 w-full max-w-md max-h-full">
+        <!-- Modal content -->
+        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <!-- Modal header -->
+            <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                    Update Notes
+                </h3>
+                <button type="button"
+                    class="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                    data-modal-hide="update-modal{{ $id }}">
+                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                        viewBox="0 0 14 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                    </svg>
+                    <span class="sr-only">Close modal</span>
+                </button>
+            </div>
+            <!-- Modal body -->
+            <div class="p-4 md:p-5">
+                <form action="{{ route('updateNote', ['id' => $id]) }}" method="POST" class="space-y-4">
+                    @csrf
+                    @method('PUT')
+                    <div>
+                        <label for="title-{{ $id }}"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                            Judul</label>
+                        <input type="text" name="title" id="title-{{ $id }}"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-slate-500 focus:border-slate-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                            placeholder="Judul Note" value="" required />
+                    </div>
+                    <div>
+                        <label for="content-{{ $id }}"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                            Konten</label>
+                        <input type="text" name="content" id="content-{{ $id }}" placeholder="Konten Note"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-slate-500 focus:border-slate-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                            value="" required />
+                    </div>
+                    <button type="submit"
+                        class="w-full text-white bg-slate-700 hover:bg-slate-800 focus:ring-4 focus:outline-none focus:ring-slate-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-slate-600 dark:hover:bg-slate-700 dark:focus:ring-slate-800">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @vite('resources/css/app.css', 'resources/js/app.js')
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ITS | Informasi Banjir
+    </title>
+
+</head>
+
+<body>
+    <div class="w-screen h-screen bg-[#F8FCFF] fMid items-center">
+        @yield('container')
+    </div>
+
+</body>
+
+</html>
